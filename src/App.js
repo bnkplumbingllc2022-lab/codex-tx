46bc2472c999460ef31262f98adcff5e7c281458
bnkplumbingllc2022-lab <bnkplumbingllc2022@gmail.com>
2026-03-22 20:05:18 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2093,7 +2093,7 @@ function LiveViewfinder({ onCapture, onFallback, isOnline, t, lang }) {
   }
 
   return (
-    <div style={{ position: "relative", borderRadius: 14, overflow: "hidden", background: "#000", aspectRatio: "4/3", maxHeight: 280 }}>
+    <div style={{ position: "relative", borderRadius: 14, overflow: "hidden", background: "#000", aspectRatio: "4/3" }}>
       <video ref={videoRef} autoPlay playsInline muted style={{ width: "100%", height: "100%", objectFit: "cover", display: camReady ? "block" : "none" }} />
       <canvas ref={canvasRef} style={{ display: "none" }} />
       {!camReady && (
